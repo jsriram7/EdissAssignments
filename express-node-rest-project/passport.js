a6ee4b569b7301127ac7288a2a60be209c11d9f5
jsriram7 <jsriram7@gmail.com>
2017-06-04 20:24:52 -0400

    second run- refactored server.js and passport.js

--- express-node-rest-project/passport.js
+++ express-node-rest-project/passport.js
@@ -2,24 +2,24 @@ var LocalStrategy   = require('passport-local').Strategy;
 
 var mysql = require('mysql');
 
+//var connection = mysql.createConnection({
+  //                host     : 'localhost',
+    //              user     : 'root',
+      //            password : 'admin',
+                  
+        //        });
 var connection = mysql.createConnection({
                   host     : 'mysql-useast1-instance.ce2fvdeklmyb.us-east-1.rds.amazonaws.com',
                   user     : 'sjaikris',
                   password : 'Madhumitha&7',
                   
                 });
-
 connection.query('USE edissdevdb');
 
-// expose this function to our app using module.exports
+
 module.exports = function(passport) {
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
+   
     passport.serializeUser(function(user, done) {
    done(null, user);
 });
@@ -30,12 +30,12 @@ passport.deserializeUser(function(user, done) {
  
 
  passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
+        
         usernameField : 'username',
         passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
+        passReqToCallback : true 
     },
-    function(req, username, password, done) { // callback with email and password from our form
+    function(req, username, password, done) { 
 
          connection.query("SELECT * FROM `users` WHERE `username` = '" + username + "'",function(err,rows){
 			 console.log("USERNAME  "+ username);
@@ -44,16 +44,16 @@ passport.deserializeUser(function(user, done) {
                 return done(err);
 			 if (!rows.length) {
 				 console.log("Username doesnot exist");
-                return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'}); // req.flash is the way to set flashdata using connect-flash
+                return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'}); // req.flash is the 
             } 
 			
-			// if the user is found but the password is wrong
+			
             if (!( rows[0].password == password))
 			{
 			console.log("Password is wrong");	             
-			 return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'}); // create the loginMessage and save it to session as flashdata
+			 return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'}); // create the 
 			}
-            // all is well, return successful user
+            
             return done(null, rows[0]);			
 		
 		});
--- express-node-rest-project/server.js
+++ express-node-rest-project/server.js
@@ -11,31 +11,31 @@ var validate = require('validator');
 
 
 require('./passport')(passport); 
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
+app.use(morgan('dev')); 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
  
-// required for passport
+// Session
 app.use(session({
 	secret: 'testpassport',
 	resave: true,
 	cookie: { maxAge: 900000 },
 	rolling: true,
 	saveUninitialized: true
- } )); // session secret
+ } )); 
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(passport.session()); 
+app.use(flash()); 
 
+//Login
 app.post('/login', function(req, res, next) {
   passport.authenticate('local-login', function(err, user, info) {
     if (err) { return next(err) }
     if (!user) {
-      // *** Display message without using flash option
-      // re-render the login form with a message
+      
 	  res.setHeader('Content-Type', 'application/json');
       return res.send({ message: info.message })
     }
@@ -48,13 +48,14 @@ app.post('/login', function(req, res, next) {
   })(req, res, next);
 });	
 
-app.get('/logout', function (req, res){
+//Logout
+app.post('/logout', function (req, res){
 	console.log(req.user);
 	if(req.user){
 	req.session.destroy(function (err) {
 	  console.log(req.user);
 	res.setHeader('Content-Type', 'application/json');
-    res.send({"message":"You have been successfully logged out"}); //Inside a callback… bulletproof!
+    res.send({"message":"You have been successfully logged out"}); 
   });	
 	}
 	else{
@@ -64,6 +65,7 @@ app.get('/logout', function (req, res){
   
 });
 
+//Ensure user is logged in
 function ensureAuthenticated(req,res,next){
 	if(req.isAuthenticated()){
 		return next();
@@ -74,50 +76,51 @@ function ensureAuthenticated(req,res,next){
 	}
 }
 
+//Add
 app.post('/add', ensureAuthenticated, function (req, res){
 	console.log(req.body.num1 + " " + req.body.num2);
-	if(!validate.isNumeric(req.body.num1) || !validate.isNumeric(req.body.num2) || validate.isEmpty(req.body.num1) || validate.isEmpty(req.body.num2)){
+	
+	if(isNaN(Number(req.body.num1)) || isNaN(Number(req.body.num2)) || !(req.body.num1) || !(req.body.num2)){
 		res.setHeader('Content-Type', 'application/json');
 		res.send({"message":"The numbers you entered are not valid"});
 	}
 	else{
-	var result= +req.body.num1 + +req.body.num2;
+	var result= Number(req.body.num1) + Number(req.body.num2);
 	res.setHeader('Content-Type', 'application/json');
 	res.send({"message":"The action was successful", "result": result});
 	}
 });
 
+//Multiply
 app.post('/multiply', ensureAuthenticated, function (req, res){
 	console.log(req.body.num1 + " " + req.body.num2);
-	if(!validate.isNumeric(req.body.num1) || !validate.isNumeric(req.body.num2) || validate.isEmpty(req.body.num1) || validate.isEmpty(req.body.num2)){
+	if(isNaN(Number(req.body.num1)) || isNaN(Number(req.body.num2)) || !(req.body.num1) || !(req.body.num2)){
 		res.setHeader('Content-Type', 'application/json');
 		res.send({"message":"The numbers you entered are not valid"});
 	}
 	else{
-	var result= req.body.num1 * req.body.num2;
+	var result= Number(req.body.num1) * Number(req.body.num2);
 	res.setHeader('Content-Type', 'application/json');
 	res.send({"message":"The action was successful", "result": result});
 	}
 });
 
+//Divide
 app.post('/divide', ensureAuthenticated, function (req, res){
 	console.log(req.body.num1 + " " + req.body.num2);
-	if(!validate.isNumeric(req.body.num1) || !validate.isNumeric(req.body.num2) || validate.isEmpty(req.body.num1) || validate.isEmpty(req.body.num2)  ||  (req.body.num2 == 0)){
+	if(isNaN(Number(req.body.num1)) || isNaN(Number(req.body.num2)) || !(req.body.num1) || !(req.body.num2) ||  (req.body.num2 == 0)){
 		res.setHeader('Content-Type', 'application/json');
 		res.send({"message":"The numbers you entered are not valid"});
 	}
 	else{
-	var result= req.body.num1 / req.body.num2;
+	var result= Number(req.body.num1) / Number(req.body.num2);
 	res.setHeader('Content-Type', 'application/json');
 	res.send({"message":"The action was successful", "result": result});
 	}
 });
 
 
-// port must be set to 8080 because incoming http requests are routed from port 80 to port 3000
+// port must be set to 3000 because incoming http requests are routed from port 80 to port 3000
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
 });
-
-
-
