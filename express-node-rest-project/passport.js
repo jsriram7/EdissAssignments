92b6c664cc8bf8fb1e77b97195119c95fe9a865f
jsriram7 <jsriram7@gmail.com>
2017-06-04 21:26:36 -0400

    refactoring the code and adding comments

--- express-node-rest-project/passport.js
+++ express-node-rest-project/passport.js
@@ -8,6 +8,8 @@ var mysql = require('mysql');
       //            password : 'admin',
                   
         //        });
+
+		//Connect to the mysql instance
 var connection = mysql.createConnection({
                   host     : 'mysql-useast1-instance.ce2fvdeklmyb.us-east-1.rds.amazonaws.com',
                   user     : 'sjaikris',
@@ -19,16 +21,17 @@ connection.query('USE edissdevdb');
 
 module.exports = function(passport) {
 
-   
+   //SerializeUser
     passport.serializeUser(function(user, done) {
    done(null, user);
 });
 
+//DeserializeUser
 passport.deserializeUser(function(user, done) {
   done(null, user);
 });
  
-
+//Login Local Strategy
  passport.use('local-login', new LocalStrategy({
         
         usernameField : 'username',
@@ -44,14 +47,14 @@ passport.deserializeUser(function(user, done) {
                 return done(err);
 			 if (!rows.length) {
 				 console.log("Username doesnot exist");
-                return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'}); // req.flash is the 
+                return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'});  
             } 
 			
 			
             if (!( rows[0].password == password))
 			{
 			console.log("Password is wrong");	             
-			 return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'}); // create the 
+			 return done(null, false, {message:'There seems to be an issue with the username/password combination that you entered'}); 
 			}
             
             return done(null, rows[0]);			
